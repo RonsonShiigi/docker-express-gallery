6521c84e6c4480ec7b727a8466269e2c18e10c9a
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2019-03-14 15:30:10 -1000

    check me please :D

--- knexfile.js
+++ knexfile.js
@@ -1,23 +1,24 @@
 // Update with your config settings.
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-  client: 'postgresql',
+  client: "postgresql",
   connection: {
-    host: process.env.POSTGRES_HOSTNAME,
+    port: process.env.POSTGRES_HOST_PORT,
+    // host: process.env.POSTGRES_HOSTNAME,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
+    database: process.env.POSTGRES_DB
   },
   pool: {
     min: 2,
     max: 10
   },
   migrations: {
-    directory: './database/migrations',
-    tableName: 'knex_migrations'
+    directory: "./database/migrations",
+    tableName: "knex_migrations"
   },
   seeds: {
-    directory: './database/seeds'
+    directory: "./database/seeds"
   }
 };
--- server.js
+++ server.js
@@ -1,48 +1,59 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const User = require('./database/models/User');
+const express = require("express");
+const bodyParser = require("body-parser");
+const User = require("./database/models/User");
 
 // data vars
-const PORT = process.env.PORT;
+const PORT = 8080;
 const SESSION_SECRET = process.env.SESSION_SECRET;
 const REDIS_HOSTNAME = process.env.REDIS_HOSTNAME;
 
-if (!PORT) { console.log('No Port Found'); }
-if (!SESSION_SECRET) { console.log('No Session Secret Found'); }
-if (!REDIS_HOSTNAME) { console.log('No Redis Hostname Found'); }
-if (!PORT || !SESSION_SECRET || !REDIS_HOSTNAME) { return process.exit(1); }
+if (!PORT) {
+  console.log("No Port Found");
+}
+if (!SESSION_SECRET) {
+  console.log("No Session Secret Found");
+}
+if (!REDIS_HOSTNAME) {
+  console.log("No Redis Hostname Found");
+}
+if (!PORT || !SESSION_SECRET || !REDIS_HOSTNAME) {
+  return process.exit(1);
+}
 
 // setup server middleware
 const app = express();
 app.use(bodyParser.json({ extended: true }));
 
 // routes
-app.get('/api/smoke', (req, res) => {
-  res.json({ smoke: 'test' });
+app.get("/api/smoke", (req, res) => {
+  res.json({ smoke: "test" });
 });
 
-app.get('/api/users', (req, res) => {
-  return new User().fetchAll()
-    .then((users) => {
+app.get("/api/users", (req, res) => {
+  return new User()
+    .fetchAll()
+    .then(users => {
+      console.log("hi");
       return res.json(users);
     })
-    .catch((err) => {
-      console.log(err);
+    .catch(err => {
+      console.log("hi", err);
       res.sendStatus(500);
     });
 });
 
-app.post('/api/users', (req, res) => {
+app.post("/api/users", (req, res) => {
   const username = req.body.username;
-  return new User({ username }).save()
-    .then((user) => {
+  return new User({ username })
+    .save()
+    .then(user => {
       return res.json({ success: true });
     })
-    .catch((err) => {
+    .catch(err => {
       console.log(err);
       res.sendStatus(500);
     });
-})
+});
 
 // start server
 app.listen(PORT, () => {
